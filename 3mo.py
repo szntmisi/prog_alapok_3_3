c9e9cc5f32e88424b910c5b817be16a75eaec5b0
szntmisi <166813050+szntmisi@users.noreply.github.com>
2024-04-19 11:58:54 +0200

    Harmadik feladat :100:

--- 3mo.py
+++ 3mo.py
@@ -25,3 +25,17 @@ for i in range(len(csigak)):
         db += 1
     
 print(f"{db} db meztelen található az adattok között")
+
+be_csiga = input("Kérem a csiga nevét")
+def kereses(csigak,s):
+    i = 0 
+    while i < len(csigak) and not (csigak[i]['latin'].lower) == s.lower():
+        i += 1
+        if i < len(csigak):
+            return csigak[i]['magyar']
+        else:
+            return "Nincs ilyen latin nevű a lsitában"
+    
+    
+print(f"A csiga magyar neve: \n {kereses(csigak,be_csiga)}")
+
